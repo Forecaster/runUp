03c72a0f955906a87443595af45b70282acb2cce
Forecaster <martinek90@hotmail.com>
2013-12-21 13:49:42 +0100

    Changed version to release

--- src/main/java/forecaster/runup/lib/Reference.java
+++ src/main/java/forecaster/runup/lib/Reference.java
@@ -3,5 +3,5 @@ package forecaster.runup.lib;
 public class Reference {
   public static final String MOD_ID = "runup";
   public static final String MOD_NAME = "RunUp";
-  public static final String VERSION = "0.0.1";
+  public static final String VERSION = "1.0";
 }
